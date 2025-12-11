d09ccc10f46f0bb99a3b6cef7d9aa886e9b0a13d
y.yes <y.yes@andersenlab.com>
2025-12-11 16:35:02 +0500

    fix

--- pkg/notifier/notifier.go
+++ pkg/notifier/notifier.go
@@ -3,7 +3,10 @@ package notifier
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/ZhanibekTau/go-sdk/pkg/notifier/errors"
 )
 
 type TelegramNotifier struct {
@@ -18,16 +21,32 @@ func NewTelegramNotifier(token, chatID string) *TelegramNotifier {
 	}
 }
 
-func (t *TelegramNotifier) Send(message string) error {
-	body := map[string]string{
-		"chat_id": t.ChatID,
-		"text":    message,
+func (t *TelegramNotifier) Send(service string, method string, message string, env string, err error) error {
+	appErr := &errors.AppError{
+		Service: service,
+		Method:  method,
+		Message: message,
+		Env:     env,
+		Err:     err,
 	}
 
-	jsonBody, _ := json.Marshal(body)
+	body := map[string]string{
+		"chat_id": t.ChatID,
+		"text":    appErr.Error(),
+	}
 
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return fmt.Errorf("failed to marshal body %v", err)
+	}
 	url := "https://api.telegram.org/bot" + t.Token + "/sendMessage"
 
-	_, err := http.Post(url, "application/json", bytes.NewBuffer(jsonBody))
-	return err
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return fmt.Errorf("failed to send error %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	return nil
 }
